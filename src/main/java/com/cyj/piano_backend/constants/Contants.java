dad8599923f8322d4f4e48903d484ed64bf3c663
畅英杰 <cyj142602@163.com>
2021-01-06 17:27:31 +0800

    登录相关

---
+++ src/main/java/com/cyj/piano_backend/constants/Contants.java
@@ -0,0 +1,16 @@
+package com.cyj.piano_backend.constants;
+
+/**
+ * @author changyingjie
+ */
+public class Contants {
+
+    public static final String APPID = "wxcf5c5717d276a8d8";
+    public static final String SECRET = "dc665fc82307079e428c5e8be8e8a5f3";
+
+    public static final String BASE_URL = "https://api.weixin.qq.com";
+
+    public static final String ACCESS_TOKEN_URL = "/cgi-bin/token?grant_type=client_credential&appid=wxcf5c5717d276a8d8&secret=dc665fc82307079e428c5e8be8e8a5f3";
+
+    public static final String LOGIN_URL = "/sns/jscode2session?grant_type=authorization_code";
+}
--- src/main/java/com/cyj/piano_backend/ctrl/LoginCtrl.java
+++ src/main/java/com/cyj/piano_backend/ctrl/LoginCtrl.java
@@ -1,11 +1,12 @@
 package com.cyj.piano_backend.ctrl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.cyj.piano_backend.constants.Contants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,11 +19,18 @@ public class LoginCtrl {
 
     private Logger logger = LoggerFactory.getLogger(LoginCtrl.class);
 
-    @PostMapping("/getLoginUser")
-    public void getLoginUser(@RequestBody Map<String, String> params) {
-        logger.info(params.get("code"));
-        logger.info(params.get("appid"));
-        logger.info(params.get("secret"));
-        logger.info(params.get("grant_type"));
+    @GetMapping("/getLoginUser")
+    public String getLoginUser(@RequestParam String code) {
+        String loginUrl = Contants.BASE_URL + Contants.LOGIN_URL;
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("appid", Contants.APPID);
+        paramsMap.put("secret", Contants.SECRET);
+        paramsMap.put("js_code", code);
+        String result = HttpUtil.get(loginUrl, paramsMap);
+        JSONObject jsonObject = JSONUtil.parseObj(result);
+        String session_key = (String) jsonObject.get("session_key");
+        String openid = (String) jsonObject.get("openid");
+        System.out.println(jsonObject);
+        return "返回值";
     }
 }
