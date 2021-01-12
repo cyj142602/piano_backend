0da16fd2565c7d2e72c0b5b1866b0bec3561250c
畅英杰 <cyj142602@163.com>
2021-01-12 17:33:06 +0800

    修改提示语

--- src/main/java/com/cyj/piano_backend/ctrl/VideoCtrl.java
+++ src/main/java/com/cyj/piano_backend/ctrl/VideoCtrl.java
@@ -1,5 +1,6 @@
 package com.cyj.piano_backend.ctrl;
 
+import cn.hutool.core.io.FileUtil;
 import com.cyj.piano_backend.bean.JsonResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -26,43 +28,31 @@ public class VideoCtrl {
 
     @ApiOperation(value = "视频上传", httpMethod = "POST")
     @RequestMapping(value = "/uploadFile", produces = "application/json;charset=UTF-8")
-    public JsonResult<String> uploadFile(@RequestParam("fileName") MultipartFile file) {
-        System.out.print("上传文件===" + "\n");
-        //判断文件是否为空
+    public JsonResult<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        logger.info("打卡视频开始上传...");
         if (file.isEmpty()) {
             return new JsonResult<>(401, "上传文件不可为空");
         }
-        // 获取文件名
+        //获取文件名，微信小程序组件会处理文件名，变成12位随机英文数字+32位随机英文数字，同一份文件的后32位相同
         String fileName = file.getOriginalFilename();
-//        System.out.print("上传的文件名为: "+fileName+"\n");
-        fileName = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + "_" + fileName;
-        System.out.print("（加个时间戳，尽量避免文件名称重复）保存的文件名为: " + fileName + "\n");
-        //加个时间戳，尽量避免文件名称重复
-        String path = "D:/fileUpload/" + fileName;
-        //String path = "E:/fileUpload/" + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + "_" + fileName;
-        //文件绝对路径
-        System.out.print("保存文件绝对路径" + path + "\n");
-        //创建文件路径
+        assert fileName != null;
+        String headFileName = fileName.substring(0, 12);
+        String tailFileName = fileName.substring(12);
+        String path = "D:/fileUpload/" + tailFileName;
         File dest = new File(path);
-        //判断文件是否已经存在
         if (dest.exists()) {
+            logger.error("打卡视频上传失败！文件已经存在");
             return new JsonResult<>(401, "文件已经存在");
-        }
-        //判断文件父目录是否存在
-        if (!dest.getParentFile().exists()) {
-            dest.getParentFile().mkdir();
         }
         String url;
         try {
-            //上传文件
             file.transferTo(dest); //保存文件
-            System.out.print("保存文件路径" + path + "\n");
             //url="http://你自己的域名/项目名/images/"+fileName;//正式项目
             url = "http://localhost:8080/images/" + fileName;//本地运行项目
 //            int jieguo= shiPinService.insertUrl(fileName,path, url);
-//            System.out.print("插入结果"+jieguo+"\n");
-            System.out.print("保存的完整url====" + url + "\n");
+            logger.info("打卡视频上传结束！path={}", path);
         } catch (IOException e) {
+            logger.error("打卡视频上传失败！msg = {}", Arrays.toString(e.getStackTrace()));
             return new JsonResult<>(401, "上传失败");
         }
         return new JsonResult<>(200, "上传成功,文件url==" + url);
